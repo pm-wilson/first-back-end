c9b61a5d4fb879a8f6ff229aa1f1cb2c36ba89d2
Patrick Wilson <pmwilson333@gmail.com>
2020-08-11 10:40:08 -0700

    pushing to heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 const geoData = require('./data/geo.js');
 const weatherData = require('./data/weather.js');
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
